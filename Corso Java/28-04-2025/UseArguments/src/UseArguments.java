b33942d007965c9de93407f64fc3c267da168042
paolorusso03 <lamiaepocalme@gmail.com>
2025-04-28 15:18:26 +0200

    Add files via upload

---
+++ Corso Java/28-04-2025/UseArguments/src/UseArguments.java
@@ -0,0 +1,10 @@
+public class UseArguments {
+    public static void main(String[] args) throws Exception {
+        
+        String pippo= "pippo";
+        
+        System.out.print("Hi, ");
+        System.out.print(pippo);
+        System.out.println(". How are you?");
+    }
+}
